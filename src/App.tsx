ce9ed4dedc04882220ac410566f2ead8095afe50
Alex Rudoy <rudoy4ik@gmail.com>
2021-02-06 11:18:41 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      {/* <Header />
+      <Suspense fallback={<LoadingIcon />}>
+        <Switch></Switch>
+      </Suspense>
+      <Footer /> */}
+    </BrowserRouter>
+  );
+}
+
+export default App;
